d38efb70392b589e1dfaab30301ff6b875ac3bd5
Ridwan-Suhel <ridwansuhel96@gmail.com>
2022-05-14 23:26:19 +0600

    available booking api added

--- index.js
+++ index.js
@@ -26,12 +26,55 @@ async function run() {
     const doctorsPortalCollection = client
       .db("doctors_portal")
       .collection("service");
+    const bookingCollection = client.db("doctors_portal").collection("booking");
 
     app.get("/service", async (req, res) => {
       const query = {};
       const cursor = doctorsPortalCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
+    });
+
+    app.get("/available", async (req, res) => {
+      const date = req.query.date;
+
+      // step 1 get all services
+      const services = await doctorsPortalCollection.find().toArray();
+
+      // step 2 get the booking of th day
+      const query = { date: date };
+      const bookings = await bookingCollection.find(query).toArray();
+
+      // step 3 for each service, find bookings for that service
+      services.forEach((service) => {
+        const serviceBookings = bookings.filter(
+          (b) => b.treatment === service.name
+        );
+        const booked = serviceBookings.map((s) => s.slot);
+        const available = service.slots.filter(
+          (slot) => !booked.includes(slot)
+        );
+        service.slots = available;
+      });
+
+      res.send(services);
+    });
+
+    app.post("/booking", async (req, res) => {
+      const booking = req.body;
+      const query = {
+        treatment: booking.treatment,
+        date: booking.date,
+        patient: booking.patient,
+      };
+
+      const exists = await bookingCollection.findOne(query);
+      if (exists) {
+        return res.send({ success: false, booking: exists });
+      }
+
+      const result = await bookingCollection.insertOne(booking);
+      return res.send({ success: true, result });
     });
   } finally {
     // await client.close();
